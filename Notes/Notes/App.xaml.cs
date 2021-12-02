5dc13ccc450eeee241d2457a8f3704c30b269ac6
Sonya <Sonya@LAPTOP-NTAU5QUE>
2021-12-02 21:38:04 +0300

    Обновить исходящее сообщение

--- Notes/Notes/App.xaml.cs
+++ Notes/Notes/App.xaml.cs
@@ -1,9 +1,25 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.IO;
+using Notes.Data;
+using Xamarin.Forms;
 
 namespace Notes
 {
     public partial class App : Application
     {
+        static NoteDatabase database;
+
+        public static NoteDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new NoteDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+                }
+                return database;
+            }
+        }
 
         public App()
         {
--- Notes/Notes/Views/AboutPage.xaml.cs
+++ Notes/Notes/Views/AboutPage.xaml.cs
@@ -13,7 +13,6 @@ namespace Notes.Views
 
         async void OnButtonClicked(object sender, EventArgs e)
         {
-            // Launch the specified URL in the system browser.
             await Launcher.OpenAsync("https://aka.ms/xamarin-quickstart");
         }
     }
